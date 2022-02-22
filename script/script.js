09a8de09c1b94dba22d6f96a7e7559635c85800a
rivaldo souza <rivaldosilva@si.fiponline.edu.br>
2022-02-21 21:38:24 -0300

    campo de busca ok

--- script/script.js
+++ script/script.js
@@ -90,7 +90,7 @@ function deletar(descricao){
 //---------------------------------------------------------------- axios----------------------------------------------------------------------
 //carregarTabela();
 
-//coletar()
+coletar()
 
 tbody.addEventListener('dblclick', evento =>{
   //carregarSelect();
